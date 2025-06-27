40a9187e615c66c7d95618c5dd9fd69dc6a8c670
FancyCherepahe <elektrodanulo@gmail.com>
2025-06-27 20:01:06 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,11 +3,3 @@ audio.loop = true;
 audio.volume = 0.5;
 let isPlaying = true;
 let random_color_text = document.getElementById("random_color_text");
-function toggleAudio() {
-    if (isPlaying) {
-        audio.pause();
-    } else {
-        audio.play();
-    }
-    isPlaying = !isPlaying;
-}
