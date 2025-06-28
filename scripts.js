f86ef53cbddc40082382fa20eec63a96e9be9517
FancyCherepahe <elektrodanulo@gmail.com>
2025-06-28 21:27:39 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-const audio = new Audio('glitch-sound-333220.mp3');
-audio.loop = true;
-audio.volume = 0.5;
-let isPlaying = true;
-let random_color_text = document.getElementById("random_color_text");
+document.addEventListener('DOMContentLoaded', function () {
+    // Sound setup
+    const soundButton = document.querySelector('.song_button');
+    const audio = new Audio('glitch-sound-333220.mp3');
+    audio.loop = true;
+    audio.volume = 0.5;
+});
